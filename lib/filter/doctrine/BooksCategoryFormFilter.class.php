eaa8d94e5a8fefd559c17719e53876b91755af37
Juan <j_dalmasso@hotmail.com>
2012-09-23 03:46:16 -0300

    Agrego lo de Librería

---
+++ lib/filter/doctrine/BooksCategoryFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * BooksCategory filter form.
+ *
+ * @package    Prueba
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class BooksCategoryFormFilter extends BaseBooksCategoryFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/BooksCategoryForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * BooksCategory form.
+ *
+ * @package    Prueba
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class BooksCategoryForm extends BaseBooksCategoryForm
+{
+  public function configure()
+  {
+  }
+}
